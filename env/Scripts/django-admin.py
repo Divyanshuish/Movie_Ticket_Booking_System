189d3eb8684bb8cf5d4f6862e9acb541096ba43e
Divyanshu Tyagi <sanjay16tyagi.st@gmail.com>
2020-04-27 01:16:20 +0530

    movie_ticketbooking_system

---
+++ env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!i:\python_django_flask\movie_ticketbooking_system\env\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
